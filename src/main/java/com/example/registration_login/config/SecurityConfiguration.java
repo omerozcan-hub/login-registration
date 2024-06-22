cf3d3b05fe722c1b9c164d46f3dbcea360dc997d
omerozcan-hub <ozcanomer15429@gmail.com>
2024-06-22 17:43:21 +0300

    thymeleaf created

--- src/main/java/com/example/registration_login/config/SecurityConfiguration.java
+++ src/main/java/com/example/registration_login/config/SecurityConfiguration.java
@@ -1,10 +1,63 @@
 package com.example.registration_login.config;
 
+import com.example.registration_login.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration{
+
+    @Autowired
+    private UserService userService;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Autowired
+    protected void configurer(AuthenticationManagerBuilder auth) throws Exception{
+        auth.authenticationProvider(authenticationProvider());
+    }
+
+    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+            .authorizeHttpRequests(authorize -> authorize
+                    .requestMatchers("/registration**", "/js/**", "/css/**", "/img/**").permitAll()
+                    .anyRequest().authenticated()
+            )
+            .formLogin(form -> form
+                    .loginPage("/login")
+                    .permitAll()
+            )
+            .logout(logout -> logout
+                    .invalidateHttpSession(true)
+                    .clearAuthentication(true)
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                    .logoutSuccessUrl("/login?logout")
+                    .permitAll()
+            );
+
+        return http.build();
+    }
+
 }
--- src/main/java/com/example/registration_login/controller/MainController.java
+++ src/main/java/com/example/registration_login/controller/MainController.java
@@ -1,10 +1,39 @@
 package com.example.registration_login.controller;
 
+import com.example.registration_login.dto.UserRegistrationDto;
+import com.example.registration_login.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
+
+    @Autowired
+    private UserService userService;
+
+    public MainController(UserService userService){
+        super();
+        this.userService = userService;
+    }
+
+    @ModelAttribute("user")
+    public UserRegistrationDto userRegistrationDto(){
+        return new UserRegistrationDto();
+    }
+
+    @GetMapping
+    public String showRegistrationForm(){
+        return "registration";
+    }
+
+    @PostMapping
+    public String registerUserAccount(@ModelAttribute("user") UserRegistrationDto registrationDto){
+        userService.save(registrationDto);
+        return "redirect:/registration?success";
+    }
 
     @GetMapping("/login")
     public String login() {
@@ -15,5 +44,7 @@ public class MainController {
     public String home() {
         return "index";
     }
+
+
 
 }
